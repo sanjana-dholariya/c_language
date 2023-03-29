c98d9826fac99a3a4542ef97d27d3a32166e3e0e
sanjana-dholariya <128052020+sanjana-dholariya@users.noreply.github.com>
2023-03-29 16:12:08 +0530

    Add files via upload

---
+++ WL(4).C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+   int n;
+   clrscr();
+   printf("enter the N:");
+   scanf("%d",&n);
+   while(n>=1)
+   {
+     printf("  %d",n);
+     n--;
+
+
+   }
+   getch();
+
+}
