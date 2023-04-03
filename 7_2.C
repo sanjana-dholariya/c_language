09de28ec597cb7950b1ac30fd23f3da8e906c263
sanjana-dholariya <128052020+sanjana-dholariya@users.noreply.github.com>
2023-04-03 18:47:11 +0530

    Add files via upload

---
+++ 7_2.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  int j,i;
+  clrscr();
+  for(i=1;i<=5;i++)
+  {
+   for(j=i;j>=1;j--)
+   {
+      printf("%d",j);
+
+   }
+   printf("\n");
+
+
+  }
+  getch();
+
+
+
+
+
+}
